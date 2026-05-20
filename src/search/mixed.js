335ea1aa800be478f10c7c8a4b10ef1dc6deef31
Poky <poky0dev@gmail.com>
2026-05-20 09:34:40 -0300

    Update mixed.js

--- src/search/mixed.js
+++ src/search/mixed.js
@@ -24,7 +24,7 @@ const simplify = (node) => {
 
 export default async function search(query, page = 0) {
   const resp = await braveFetch(
-    `https://search.brave.com/search?lang=pt-br&q=${encodeURIComponent(
+    `https://search.brave.com/search?search_lang=pt&q=${encodeURIComponent(
       query,
     )}${page ? `&offset=${page}` : ""}&source=web`,
     {
