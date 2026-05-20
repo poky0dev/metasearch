6155033d869525f81b99abc311fe751add8b2fc6
Poky <poky0dev@gmail.com>
2026-05-20 09:56:59 -0300

    Update braveFetch.js

--- src/search/braveFetch.js
+++ src/search/braveFetch.js
@@ -12,7 +12,7 @@ const braveFetch = async (url, options = {}) => {
 		"sec-fetch-site": "same-origin",
 		"sec-fetch-user": "?1",
 		"sec-gpc": "1",
-		"accept-language": "en-US, en, pt-BR;q=0.9",
+		"accept-language": "en-US, en;q=0.9",
 		"cache-control": "max-age=0",
 		...options.headers,
 	};
